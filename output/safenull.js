02f38c1d9e5aaa88d9dd9b06abadb390840fcdcd
Sebastian Mayr <sebmaster16@gmail.com>
2014-02-27 19:42:30 +0100

    Implement safe navigation operator

---
+++ output/safenull.js
@@ -0,0 +1,6 @@
+﻿var x = {
+	a: { d: 'b' },
+	c: 'd'
+};
+console.log(typeof x.a === 'undefined' ? undefined : x.a.d);
+console.log(typeof (typeof x === 'undefined' ? undefined : x.y) === 'undefined' ? undefined : (typeof x === 'undefined' ? undefined : x.y).z);
---
+++ safenull.sjs
@@ -0,0 +1,9 @@
+﻿macro ?. {
+	rule infix { $obj (.) ... | $rest } => {
+		(typeof $obj (.) ... === 'undefined' ? undefined : $obj (.) .... $rest)
+	}
+}
+
+var x = {a:{d: 'b'}, c:'d'};
+console.log(x.a?.d);
+console.log(x?.y?.z);
